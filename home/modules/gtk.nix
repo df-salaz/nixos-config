a692e93b8044dde3901963b6bb7fe2f14902dc95
David <df.salaz@gmail.com>
2024-04-16 02:33:53 -0700

    Pull out QT

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -19,4 +19,9 @@
 		gtk3.extraConfig.Settings = ''gtk-application-prefer-dark-theme = 1;'';
 		gtk4.extraConfig.Settings = ''gtk-application-prefer-dark-theme = 1;'';
 	};
+
+	qt = {
+		enable = true;
+		platformTheme = "gtk3";
+	};
 }
