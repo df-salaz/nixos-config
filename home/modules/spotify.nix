e000e5f0b0785be5d67728c2edc84cc1795af516
David <df.salaz@gmail.com>
2024-04-16 18:20:48 -0700

    Spicetify

--- home/modules/spotify.nix
+++ home/modules/spotify.nix
@@ -1,6 +1,16 @@
 { pkgs, ...}:
 {
+	home.file = {
+		".config/spicetify/Themes/catppuccin".source = 
+			../config/spicetify/Themes/catppuccin;
+	};
+	# imperative instructions:
+	# `spicetify config current_theme catppuccin`
+	# `spicetify config color_scheme mocha`
+	# `spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite assets_1`
+	# `spicetify apply`
 	home.packages = with pkgs; [
 		nur.repos.nltch.spotify-adblock
+		spicetify-cli
 	];
 }
