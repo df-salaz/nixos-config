e0202fd720959be234af76e9e0c86cc1a13431f9
David <df.salaz@gmail.com>
2024-04-23 13:15:17 -0700

    NixVim

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -1,34 +1,37 @@
 { userSettings, pkgs, ... }:
 
 {
-	programs.nixvim = {
-		enable = true;
-		clipboard.register = "unnamedplus";
-		colorschemes.catppuccin.enable = true;
-		plugins = {
-			bufferline.enable = true;
-			cmp.enable = true;
-			gitsigns.enable = true;
-			lsp.enable = true;
-			lualine.enable = true;
-			lualine.globalstatus = true;
-			markdown-preview.enable = true;
-			neo-tree.enable = true;
-			neocord.enable = true; # This is so funny
-			nix.enable = true;
-			obsidian.enable = true;
-			statuscol.enable = true;
-			surround.enable = true;
-			telescope.enable = true;
-			todo-comments.enable = true;
-			toggleterm.enable = true;
-			treesitter.enable = true;
-			ts-autotag.enable = true;
-			vim-css-color.enable = true;
-			vim-matchup.enable = true;
-			vimtex.enable = true;
-		};
-		viAlias = true;
-		vimAlias = true;
-	};
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+    clipboard.register = "unnamedplus";
+    colorschemes.catppuccin.enable = true;
+    options = {
+      updatetime = 100;
+      relativenumber = true;
+    };
+    plugins = {
+      bufferline.enable = true;
+      cmp.enable = true;
+      gitsigns.enable = true;
+      lsp.enable = true;
+      lualine.enable = true;
+      lualine.globalstatus = true;
+      markdown-preview.enable = true;
+      neo-tree.enable = true;
+      nix.enable = true;
+      statuscol.enable = true;
+      surround.enable = true;
+      telescope.enable = true;
+      todo-comments.enable = true;
+      toggleterm.enable = true;
+      treesitter.enable = true;
+      ts-autotag.enable = true;
+      vim-css-color.enable = true;
+      vim-matchup.enable = true;
+      vimtex.enable = true;
+    };
+    viAlias = true;
+    vimAlias = true;
+  };
 }
