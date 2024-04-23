c59f581adfba10150e1a6e54dda7e50ce5626616
David <df.salaz@gmail.com>
2024-04-23 13:39:40 -0700

    More language server config

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -9,20 +9,38 @@
     options = {
       updatetime = 100;
       relativenumber = true;
+      cursorline = true;
+      cursorlineopt = "number";
+      tabstop = 4;
+      smartindent = true;
+      formatoptions = "tcro/qwa2nlj";
+      conceallevel = 2;
     };
     plugins = {
       bufferline.enable = true;
       cmp.enable = true;
+      cmp-nvim-lsp.enable = true;
+      fugitive.enable = true;
       gitsigns.enable = true;
-      lsp.enable = true;
+      lsp = {
+        enable = true;
+        servers = {
+          lua-ls.enable = true;
+          texlab.enable = true;
+        };
+      };
       lualine.enable = true;
       lualine.globalstatus = true;
       markdown-preview.enable = true;
       neo-tree.enable = true;
       nix.enable = true;
+      nvim-jdtls.enable = true;
       statuscol.enable = true;
       surround.enable = true;
-      telescope.enable = true;
+      telescope = {
+        enable = true;
+        "<leader>ff" = "find_files";
+      };
       todo-comments.enable = true;
       toggleterm.enable = true;
       treesitter.enable = true;
