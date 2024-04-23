1f716f32364d795779c99ce84018a48e4d918494
David <df.salaz@gmail.com>
2024-04-22 20:15:30 -0700

    Neo-tree

--- home/config/nvim/lua/plugins/outline.lua
+++ home/config/nvim/lua/plugins/outline.lua
@@ -2,7 +2,7 @@ return {
 	"hedyhli/outline.nvim",
 	config = function()
 		-- Example mapping to toggle outline
-		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+		vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR><cmd>OutlineFocus<CR>",
 		{ desc = "Toggle Outline" })
 
 		require("outline").setup {
