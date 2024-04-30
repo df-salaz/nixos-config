4a185c44f5926ac694c313843e490b39a9ffee7e
David <df.salaz@gmail.com>
2024-04-29 22:17:21 -0700

    Formatting

--- home/modules/default.nix
+++ home/modules/default.nix
@@ -1,21 +1,21 @@
 {
-	imports = [
-	# System
-		./shell.nix
-		./hyprland.nix
-		./wofi.nix
-		./waybar.nix
-	# Programs
-		#./nvim.nix
-		./nixvim.nix
-		./git.nix
-		./cava.nix
-		./games.nix
-		./tor.nix
-		./discord.nix
-		./spotify.nix
-	# Customization
-		./gtk.nix
-		./theme.nix
-	];
+  imports = [
+    # System
+    ./shell.nix
+    ./hyprland.nix
+    ./wofi.nix
+    ./waybar.nix
+    # Programs
+        #./nvim.nix
+        ./nixvim.nix
+        ./git.nix
+        ./cava.nix
+        ./games.nix
+        ./tor.nix
+        ./discord.nix
+        ./spotify.nix
+    # Customization
+    ./gtk.nix
+    ./theme.nix
+  ];
 }
