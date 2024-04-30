3e50b838c712bd962a5873d3c4213693cea76a8a
David <df.salaz@gmail.com>
2024-04-29 22:24:44 -0700

    Formatting

--- home/home.nix
+++ home/home.nix
@@ -1,56 +1,56 @@
 {config, inputs, options, userSettings, pkgs, ... }:
 
 {
-	xdg.enable = true;
+  xdg.enable = true;
 
-	imports = [
-		./modules
-	];
+  imports = [
+    ./modules
+  ];
 
-	home.username = userSettings.username;
-	home.homeDirectory = "/home/"+userSettings.username;
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/"+userSettings.username;
 
-	nixpkgs.config.allowUnfree = true;
-	home.packages = with pkgs; [
-		vlc
-		dolphin-emu
-		obsidian
-		mathematica
-		krita
-		blender
-		prismlauncher
-		gnome.gnome-calculator
-		cinnamon.nemo
-		rofimoji
-		zoom-us
-		neofetch
-		firefox
-		chromium
-	];
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    vlc
+    dolphin-emu
+    obsidian
+    mathematica
+    krita
+    blender
+    prismlauncher
+    gnome.gnome-calculator
+    cinnamon.nemo
+    rofimoji
+    zoom-us
+    neofetch
+    firefox
+    chromium
+  ];
 
-	programs = {
-		ripgrep.enable = true;
-		obs-studio.enable = true;
-		zathura.enable = true;
-		emacs.enable = true;
-	};
+  programs = {
+    ripgrep.enable = true;
+    obs-studio.enable = true;
+    zathura.enable = true;
+    emacs.enable = true;
+  };
 
-	services = {
-		easyeffects.enable = true;
-		cliphist.enable = true;
-		udiskie.enable = true;
-		udiskie.tray = "never";
-	};
+  services = {
+    easyeffects.enable = true;
+    cliphist.enable = true;
+    udiskie.enable = true;
+    udiskie.tray = "never";
+  };
 
-	# Let Home Manager install and manage itself.
-	programs.home-manager.enable = true;
+    # Let Home Manager install and manage itself.
+    programs.home-manager.enable = true;
 
-	# This value determines the Home Manager release that your configuration is
-	# compatible with. This helps avoid breakage when a new Home Manager release
-	# introduces backwards incompatible changes.
-	#
-	# You should not change this value, even if you update Home Manager. If you do
-	# want to update the value, then make sure to first check the Home Manager
-	# release notes.
-	home.stateVersion = "23.11"; # Please read the comment before changing.
-}
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    home.stateVersion = "23.11"; # Please read the comment before changing.
+  }
