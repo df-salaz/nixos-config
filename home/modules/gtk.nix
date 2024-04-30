093fc62eb39ce3041f99652919f86b650971e728
David <df.salaz@gmail.com>
2024-04-29 22:29:21 -0700

    No titlebar buttons in GTK

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -1,23 +1,29 @@
 { config, inputs, lib, pkgs, ... }:
 
 {
-	home.pointerCursor = {
-		gtk.enable = true;
-		package = lib.mkDefault pkgs.gnome.adwaita-icon-theme;
-		name = lib.mkDefault "Adwaita";
-		size = lib.mkDefault 16;
-	};
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = lib.mkDefault pkgs.gnome.adwaita-icon-theme;
+    name = lib.mkDefault "Adwaita";
+    size = lib.mkDefault 16;
+  };
 
-	gtk = {
-		enable = true;
-		iconTheme = lib.mkDefault {
-			name = "Papirus";
-			package = pkgs.papirus-icon-theme;
-		};
-	};
+  dconf.settings = {
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = ""; # No titlebar buttons
+    };
+  };
 
-	qt = {
-		enable = true;
-		platformTheme = "gtk3";
-	};
+  gtk = {
+    enable = true;
+    iconTheme = lib.mkDefault {
+      name = "Papirus";
+      package = pkgs.papirus-icon-theme;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme = "gtk3";
+  };
 }
