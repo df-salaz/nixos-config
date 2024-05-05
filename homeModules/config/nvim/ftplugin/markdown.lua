3a987d5d43fbb7579ba78110ed0f9bbbe730e6be
David <df.salaz@gmail.com>
2024-05-05 07:40:41 -0700

    Remove old NeoVim config

--- homeModules/config/nvim/ftplugin/markdown.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set("n", "<Bslash>ll", ":MarkdownPreviewToggle<CR>", { noremap = true, silent = true, buffer = true })
-vim.opt_local.spell = true
-
--- homeModules/config/nvim/lua/plugins/commentary.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tpope/vim-commentary",
-	config = function ()
-		vim.keymap.set('n', '<C-/>', '<Plug>CommentaryLine', {})
-		vim.keymap.set('i', '<C-/>', '<C-[><Plug>CommentaryLine', {})
-		vim.keymap.set('v', '<C-/>', '<Plug>Commentary', {})
-	end
-}
-
