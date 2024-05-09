eac0f74d79e9ca92c7f871fcac46ba1c565f50e5
David <df.salaz@gmail.com>
2024-05-08 23:31:02 -0700

    Switch power menu script to fuzzel

--- homeModules/config/powermenu-sway.sh
+++ homeModules/config/powermenu-sway.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-case "$(echo -e "1 -  Shut down\n2 -  Reboot\n3 -  Log out" | wofi --sort-order=alphabetical --dmenu --insensitive --prompt "Power Menu" -H 190 -W 290)" in
-	"1 -  Shut down") exec systemctl poweroff;;
-	"2 -  Reboot") exec systemctl reboot;;
-	"3 -  Log out") exec sway exit;;
+case "$(echo -e "  Shut down
+  Reboot
+  Lock
+⏼  Log out" | fuzzel -l 4 -w 20 -I -d)"
+in
+	"  Shut down") exec systemctl poweroff;;
+	"  Reboot") exec systemctl reboot;;
+	"  Lock") exec swaylock;;
+	"⏼  Log out") exec sway exit;;
 esac
--- homeModules/fuzzel.nix
+++ homeModules/fuzzel.nix
@@ -9,17 +9,28 @@
     programs.fuzzel = {
       enable = true;
       settings = {
-        main = {
-          terminal = config.terminalEmulator.defaultTerminalEmulator;
+        border = {
+          width = 2;
+          radius = 10;
         };
         colors = {
-          background = "1e1e2edd";
+          background = "1e1e2eff";
           text = "cdd6f4ff";
           match = "f38ba8ff";
-          selection = "585b70ff";
+          selection = "313244ff";
           selection-match = "f38ba8ff";
           selection-text = "cdd6f4ff";
           border = "b4befeff";
+        };
+        main = {
+          horizontal-pad = 32;
+          vertical-pad = 8;
+          inner-pad = 8;
+          width = 48;
+          lines = 16;
+          line-height = 24;
+          icon-theme = "Papirus";
+          terminal = config.terminalEmulator.defaultTerminalEmulator + " -e";
         };
       };
     };
