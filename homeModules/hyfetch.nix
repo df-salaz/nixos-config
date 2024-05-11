7033911283e2f20a3500897af7016249e0cbbfab
David <df.salaz@gmail.com>
2024-05-11 01:38:16 -0700

    Add fastfetch config

--- homeModules/hyfetch.nix
+++ homeModules/hyfetch.nix
@@ -9,6 +9,12 @@
     home.packages = with pkgs; [
       fastfetch
     ];
+    xdg = {
+      enable = true;
+      configFile."fastfetch/config.jsonc" = {
+        source = ./config/fastfetch.jsonc;
+      };
+    };
     programs.hyfetch = {
       enable = true;
       settings = {
@@ -18,7 +24,6 @@
         lightness = 0.75;
         color_align.mode = "horizontal";
         backend = "fastfetch";
-        args = "--config paleofetch";
       };
     };
   };
