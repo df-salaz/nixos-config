547ccfe450ae89ce965579d77967b67f0d0ea111
David <df.salaz@gmail.com>
2024-05-17 03:52:49 -0700

    Fix luasnip engine

--- homeModules/nixvim.nix
+++ homeModules/nixvim.nix
@@ -142,7 +142,9 @@
               "<down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
               "<up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
             };
-            snippet.expand = "luasnip";
+            snippet.expand = ''function(args) 
+              require('luasnip').lsp_expand(args.body)
+            end'';
             sources = [
               { name = "nvim_lsp"; }
               { name = "luasnip"; }
