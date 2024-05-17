0389c58ce89ec26189a8a4e240fd8b1a213a2bc8
David <df.salaz@gmail.com>
2024-05-17 03:23:43 -0700

    Add luau-lsp. Very hacky.

--- homeModules/nixvim.nix
+++ homeModules/nixvim.nix
@@ -8,6 +8,7 @@
   config = lib.mkIf config.nixvim.enable {
     home.packages = with pkgs; [
       texliveFull
+
     ];
     programs.nixvim = {
       enable = true;
@@ -240,6 +241,37 @@
       };
       viAlias = true;
       vimAlias = true;
+      extraPlugins = with pkgs; [
+        vimPlugins.plenary-nvim
+        (pkgs.vimUtils.buildVimPlugin {
+          name = "luau-lsp.nvim";
+          src = pkgs.fetchFromGitHub {
+            owner = "lopi-py";
+            repo = "luau-lsp.nvim";
+            rev = "516929172874a6d240ae9100bb85d69cf50c11e7";
+            hash = "sha256-ssvuwWT6DD6XkGzTl6MdNhUi2kRn3g6e5jZ+VqCgjKc=";
+          };
+        })
+      ];
+      extraConfigLua = ''
+        require("luau-lsp").setup {
+          plugin = {
+            enabled = true,
+            port = 3667,
+          },
+          server = {
+            settings = {
+              ["luau-lsp"] = {
+                completion = {
+                  imports = {
+                    enabled = true,
+                  },
+                },
+              },
+            },
+          },
+        }
+      '';
     };
   };
 }
