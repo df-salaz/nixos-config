22dcb15ba57b3a96aec3ae869e4b9a6619f7f9a1
David <df.salaz@gmail.com>
2024-05-21 03:17:08 -0700

    Remove unused dunst settings, set Sway borders to $base

--- homeModules/theme.nix
+++ homeModules/theme.nix
@@ -110,9 +110,9 @@
           background = "$crust";
           unfocused = {
             inherit background;
-            border = "$crust";
-            childBorder = "$crust";
-            indicator = "$crust";
+            border = "$base";
+            childBorder = "$base";
+            indicator = "$base";
             text = "$text";
           };
         in lib.mkForce {
@@ -136,24 +136,6 @@
           inherit background;
         };
       };
-    };
-    services = {
-      /* dunst.settings = lib.mkIf cat {
-        global = {
-          background = "#1e1e2e";
-          foreground = "#cdd6f4";
-          separator_color = "frame";
-        };
-        urgency_low = {
-          frame_color = "#a6e3a1";
-        };
-        urgency_normal = {
-          frame_color = "#89b4fa";
-        };
-        urgency_critical = {
-          frame_color = "#f38ba8";
-        };
-      }; */
     };
   };
 }
