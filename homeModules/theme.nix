d9ed5b458a6bb337f0c47b2e1531c8ce2f4b4359
David <df.salaz@gmail.com>
2024-05-21 03:56:01 -0700

    Simplify terminal theme config

--- homeModules/theme.nix
+++ homeModules/theme.nix
@@ -39,39 +39,9 @@
         ''@import url("https://catppuccin.github.io/discord/dist/catppuccin-${config.catppuccin.flavour}-${config.catppuccin.accent}.theme.css");'';
     };
     programs = {
-      /* alacritty = {
-        catppuccin.enable = cat;
-        settings = {
-          colors.cursor.cursor = lib.mkForce "#cdd6f4";
-        };
-      }; */
-      /* kitty = {
-        catppuccin.enable = cat;
-      }; */
       foot.settings = {
         cursor = {
           color = "111111 cccccc";
-        };
-        colors = {
-          foreground = "cdd6f4";
-          background = "1e1e2e";
-          regular0 = "45475a";
-          regular1 = "f38ba8";
-          regular2 = "a6e3a1";
-          regular3 = "f9e2af";
-          regular4 = "89b4fa";
-          regular5 = "C4A0EE";
-          regular6 = "94e2d5";
-          regular7 = "bac2de";
-          bright0 = "585b70";
-          bright1 = "f38ba8";
-          bright2 = "a6e3a1";
-          bright3 = "f9e2af";
-          bright4 = "89b4fa";
-          bright5 = "C4A0EE";
-          bright6 = "94e2d5";
-          bright7 = "a6adc8";
-          scrollback-indicator = "000000 98c379";
         };
       };
       swaylock = {
