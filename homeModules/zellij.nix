400a5f142f139b8e0abc6b746302e9a09574099a
David <df.salaz@gmail.com>
2024-05-21 04:04:38 -0700

    Disable zellij zsh integration

--- homeModules/zellij.nix
+++ homeModules/zellij.nix
@@ -7,12 +7,11 @@
   config = lib.mkIf config.zellij.enable {
     programs.zellij = {
       enable = true;
-      enableZshIntegration = true;
+      enableZshIntegration = false;
       settings = {
         on_force_close = "detach";
         simplified_ui = false;
         pane_frames = true;
-        # default_layout = "compact";
         copy_command = "${pkgs.wl-clipboard}/bin/wl-copy";
         ui.pane_frames.rounded_corners = true;
         keybinds = {
